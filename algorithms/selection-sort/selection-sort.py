354dfdf63bbe193ecef3f6ac612323dc9454f694
Akshay Sadalgekar <57189307+asadalgekar@users.noreply.github.com>
2023-08-14 13:10:46 -0500

    Create selection-sort.py

---
+++ algorithms/selection-sort/selection-sort.py
@@ -0,0 +1,14 @@
+input =[2, 3, 4, 1, 6]
+def selectionSort(arr):
+    for i in range(len(arr) - 1):
+        min_index = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+            
+        if i != min_index:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+# Unsorted Array: 2,3,4,1,6
+result = selectionSort(input) #Sorted Array: 1,2,3,4,6
