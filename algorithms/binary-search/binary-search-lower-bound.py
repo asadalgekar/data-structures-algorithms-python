f8629487701c075b03cca1de050a1334d4ed91b9
Akshay Sadalgekar <57189307+asadalgekar@users.noreply.github.com>
2023-08-18 13:00:44 -0500

    Create binary-search-lower-bound.py

---
+++ algorithms/binary-search/binary-search-lower-bound.py
@@ -0,0 +1,22 @@
+# Time: Best: O(log n)
+# Space:  O(1)
+
+def lowerBound(input_array, target_value):
+    low = 0  # Initialize low pointer
+    high = len(input_array) - 1  # Initialize high pointer
+    answer = len(input_array)  # Initialize answer as the maximum possible index
+    
+    while low <= high:                              # Binary search loop
+        mid = (low + high) // 2                     # Calculate middle index
+        
+        if input_array[mid] >= target_value:        # If mid element is greater than or equal to target
+            answer = mid                            # Update answer to mid index
+            high = mid - 1                          # Look for a smaller index on the left
+        else:
+            low = mid + 1                           # Look on the right
+            
+    return answer                                   # Return the lower bound index
+
+input_array = [-1, 0, 3, 5, 9, 12]
+target_value = 9
+result = lowerBound(input_array, target_value) # Output: 4
