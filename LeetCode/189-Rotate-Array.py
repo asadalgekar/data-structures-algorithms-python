e63952ff5ef98d12fe358cb840b323e538664303
Akshay Sadalgekar <57189307+asadalgekar@users.noreply.github.com>
2023-08-20 11:34:58 -0500

    Create 189-Rotate-Array.py

---
+++ LeetCode/189-Rotate-Array.py
@@ -0,0 +1,30 @@
+""" 
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+"""
+
+# Solution 1: Brute: Time: O(n) Space: O(n)
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        temp = [0] * len(nums)
+
+        for i in range(len(nums)):
+            temp[(i + k) % len(nums)] = nums[i]
+        
+        nums[:] = temp
+
+        return nums
+
+# Solution 2: Optimal: Time: O(n) Space: O(1)
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k=k%len(nums)
+        nums[:] = nums[n - k:] + nums[:n - k]
+        return nums
+        
