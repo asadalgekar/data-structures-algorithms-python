2d801718a591b53c21a5d2bc7923c919661deffe
Akshay Sadalgekar <57189307+asadalgekar@users.noreply.github.com>
2023-11-29 15:52:38 -0600

    Update 26. Remove Duplicates from Sorted Array.py

--- LeetCode/26. Remove Duplicates from Sorted Array.py
+++ LeetCode/26. Remove Duplicates from Sorted Array.py
@@ -1,6 +1,9 @@
 """
 Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
 The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Input: nums = [1,1,2,2,3,4]
+Output: 4, nums = [1,2,3,4,_,_]
 """
 
 # Solution: Optimal Time: O(n) Space: O(1)
